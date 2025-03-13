c7d250d7d1af9cfbb9dafc9402af059ccf97e821
sybergst <tlsckstlr1@naver.com>
2025-03-13 14:14:48 +0000

    13일 깔짝 더

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,19 @@
 # print("{0:.>30}".format("understand?"))
 
-y = 3.421534352
-print("{0:0.4f}".format(y))
+# y = 3.421534352
+# print("{0:0.4f}".format(y))
 
-name = "홍길동"
-age = 19
-print(f"나의 이름은 {name}이고 나이는 {age}입니다.")
+# name = "홍길동"
+# age = 19
+# print(f"나의 이름은 {name}이고 나이는 {age}입니다.")
+
+# 리스트는 어떤 지료형도 구성 요소로 받을 수 있다.
+a = [1, 2, 3, 4, [1, 2, 3]]
+print(a[0] + a[3])
+print(a[-1])
+print(a[-1][2])
+
+print("\n{0:=>10}".format("분리선\n"))
+
+#리스트의 슬라이싱 기법
+print(a[0:3])
